c15b7b6cfa69913c2b0d88bb6cb734f2ad3d3ce0
Patrick Knabe <p.knabe@wut.de>
2020-06-05 16:58:10 +0200

    Clock output in the set frequency

--- clock-generator.js
+++ clock-generator.js
@@ -2,66 +2,80 @@ module.exports = function( RED ) {
 
 	function ClockGenerator( config ) {
 		RED.nodes.createNode( this, config );
-console.log( config );
+
 		const node = this;
-		let interval = null;
+		let timeout;
+		let timestamp = 0;
 		let status;
 
-		if( !config.controlled ) {
-			start();
-		} else {
-			node.on( 'input', function( msg ) {
-				if( ( msg.payload === true || msg.payload === 1 ) && interval === null ) {
-					start();
-				} else if( ( msg.payload === false || msg.payload === 0 ) && interval !== null ) {
-					stop();
-				}
-			} );
-
-			node.status( { fill: 'grey', shape: 'dot', text: 'status: ---' } );
-		}
-
 		function start() {
-			status = true;
-			send();
+			if( timestamp === 0 ) {
+				timestamp = new Date().getTime();
+				status = true;
 
-			interval = setInterval( function() {
-				status = !status;
-				send();
-			}, config.frequency * 500 );
+				update();
+			}
 		}
 
 		function stop() {
-			clearInterval( interval );
-			interval = null;
+			if( timestamp !== 0 ) {
+				timestamp = 0;
+				status = false;
 
-			node.status( { fill: 'grey', shape: 'dot', text: 'status: ---' } );
+				update();
+			}
 		}
 
-		function send() {
-			let payload;
+		function update() {
+			msg = {
+				name: config.name,
+				topic: config.topic,
+				timestamp: new Date().getTime()
+			};
 
-			switch( config.output ) {
+			switch( parseInt( config.output ) ) {
 
 				case 1:
-					payload = !status ? false : true;
+					msg.payload = !status ? false : true;
 					break;
 
 				case 2:
-					payload = !status ? 0 : 1;
+					msg.payload = !status ? 0 : 1;
 					break;
 
 			}
 
-			node.send( {
-				name: config.name,
-				topic: config.topic,
-				payload: payload,
-				timestamp: new Date().getTime()
-			} );
+			node.send( msg );
 
-			node.status( { fill: 'green', shape: 'dot', text: 'status: ' + payload } );
+			if( timestamp !== 0 ) {
+				node.status( { fill: 'green', shape: 'dot', text: 'active: ' + msg.payload } );
+
+				timestamp += config.period * 500;
+				timeout = setTimeout( () => {
+					status = !status;
+					update();
+				}, timestamp - msg.timestamp );
+			} else {
+				clearTimeout( timeout );
+				node.status( { fill: 'grey', shape: 'dot', text: 'inactive' } );
+			}
 		}
+
+		if( !config.controlled ) {
+			start();
+		} else {
+			node.status( { fill: 'grey', shape: 'dot', text: 'inactive' } );
+
+			node.on( 'input', ( msg ) => {
+				if( msg.payload ) {
+					start();
+				} else {
+					stop();
+				}
+			} );
+		}
+
+		node.on( 'close', stop );
 	}
 
 	RED.nodes.registerType( 'clock-generator', ClockGenerator );
